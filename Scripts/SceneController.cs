4df16b5d01872a715257ac3c6cc85e346574a24e
anjalipandey11 <130891382+anjalipandey11@users.noreply.github.com>
2024-04-06 17:55:02 +0530

    Add files via upload

---
+++ Scripts/SceneController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Scene : MonoBehaviour
+{
+    public void SwitchScenes(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
